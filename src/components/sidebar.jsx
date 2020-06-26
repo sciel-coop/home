38c354305da3374a5e0077805371daa5ecc1a41e
Van <vanbettauer@gmail.com>
2020-06-26 15:11:56 -0400

    changed the sidebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -15,7 +15,7 @@ export default class Sidebar extends Component {
             <nav id="colorlib-main-menu" role="navigation" className="navbar">
               <div id="navbar" className="collapse">
                 <ul>
-                  <li className="active"><a href="#home" data-nav-section="home">Introduction</a></li>
+                  <li className="active"><a href="#home" data-nav-section="home">Home</a></li>
                   <li><a href="#about" data-nav-section="about">About</a></li>
                   {/*<li><a href="#" data-nav-section="projects">Projects</a></li>
                   <li><a href="#" data-nav-section="blog">Blog</a></li>*/}
