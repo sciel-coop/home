5bf4a4ca90ddcfcacd5351c9c0725dc1e4eaa0d5
Van <vanbettauer@gmail.com>
2020-06-26 16:14:53 -0400

    typos

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -14,7 +14,7 @@ export default class Introduction extends Component {
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
-                          <h1> Sinlge Cell - Informatics - Experiments - <br /> Learning</h1>
+                          <h1> Single Cell - Informatics - Experiments - <br /> Learning</h1>
                         </div>
                       </div>
                     </div>
